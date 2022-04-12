d8ca405022c63467f7034274f349c93990cda5c9
lucioclearcd <lucios25062012@gmail.com>
2022-04-12 15:22:27 -0300

    aula 08 design pattern

--- src/test/java/br/com/chronosAcademy/automacaoWeb/testeWeb.java
+++ src/test/java/br/com/chronosAcademy/automacaoWeb/testeWeb.java
@@ -1,5 +1,6 @@
 package br.com.chronosAcademy.automacaoWeb;
 
+import br.com.chronosAcademy.core.Driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
@@ -9,26 +10,29 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.IOException;
+
 import static org.junit.Assert.assertEquals;
 
 public class testeWeb {
     WebDriver driver;
+    Driver driverWeb;
+
+    //WebDriver driver;
     @Before
-    public void inicializarTeste(){
-        WebDriverManager.chromedriver().setup();
-
-
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+    public void inicializarTeste() throws IOException {
+        //WebDriverManager.chromedriver().setup();
+        driverWeb = new Driver("iedriver");
+        driver = driverWeb.getDriver();
         driver.get("https://www.chronosacademy.com.br");
-
 
     }
     @Test
     public void primeiroTeste(){
 
 
-        String xpathTitulo = "/html/body/div/div/div/div/div/section[2]/div[3]/div/div/div/div/div[1]/div/h4";
+       // String xpathTitulo = "//html/body/div/div/div/div/div/section[2]/div[3]/div/div/div/div/div[1]/div/h4";
+        String xpathTitulo = "//section[2]//h4";
         WebElement txtTitulo = driver.findElement(By.xpath(xpathTitulo));
         String titulo = txtTitulo.getText();
         assertEquals("Porque Tempo É Conhecimento",titulo);
